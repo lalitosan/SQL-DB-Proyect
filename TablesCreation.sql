107feb6e8948d7e5abe2083f9ab2eef687789240
Eduardo López Goerne Salgado <70038537+lalitosan@users.noreply.github.com>
2020-10-19 22:24:22 -0500

    Update TablesCreation.sql

--- TablesCreation.sql
+++ TablesCreation.sql
@@ -3,6 +3,6 @@ CREATE TABLE Canal_Distribucion (ID_Canal      INT PRIMARY KEY, Canal_Nombre    
 CREATE TABLE Grupo_Venta        (ID_GrupoVenta INT PRIMARY KEY, GrupoVenta_Nombre VARCHAR(45), FK_CanalDist   INT);
 CREATE TABLE Cliente            (ID_Cliente    INT PRIMARY KEY, Cliente_Nombre    VARCHAR(45), FK_GrupoVenta  INT);
 CREATE TABLE Familia_Material   (ID_Familia    INT PRIMARY KEY, Familia_Nombre    VARCHAR(45), FK_Sociedad    INT);
-CREATE TABLE Material 			(ID_Material   INT PRIMARY KEY, Material_Nombre   VARCHAR(45), FK_FamMaterial INT);
+CREATE TABLE Material 			    (ID_Material   INT PRIMARY KEY, Material_Nombre   VARCHAR(45), FK_FamMaterial INT);
 CREATE TABLE Centro_Productivo  (ID_CentroProd VARCHAR(6) PRIMARY KEY, CentroProd_Nombre VARCHAR(45), FK_CanalDist INT);
-CREATE TABLE Venta				(ID_Pedido	   INT PRIMARY KEY AUTO_INCREMENT, Importe INT, Cantidad_UME INT, Cantidad_UMB INT, Fecha DATE, FK_Sociedad INT, FK_Material INT);  
+CREATE TABLE Venta				      (ID_Pedido	   INT PRIMARY KEY AUTO_INCREMENT, Importe INT, Cantidad_UME INT, Cantidad_UMB INT, Fecha DATE, FK_Sociedad INT, FK_Material INT);  
