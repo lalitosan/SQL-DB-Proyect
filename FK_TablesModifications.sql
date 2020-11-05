1c61491083fca2550bd67de1e81dc0b80113b379
A00570852 <A00570852@itesm.mx>
2020-11-05 14:40:39 -0600

    Aun hay fallas
    Falla las FK de ID_FamMaterial, sujeto a revisión y nueva versión

--- FK_TablesModifications.sql
+++ FK_TablesModifications.sql
@@ -1,13 +1,8 @@
-ALTER TABLE Canal_Distribucion ADD CONSTRAINT sociedad_FK     FOREIGN KEY (FK_Sociedad)    REFERENCES Sociedad (ID_Sociedad); 
-ALTER TABLE Grupo_Venta 	   ADD CONSTRAINT canaldist_FK    FOREIGN KEY (FK_CanalDist)   REFERENCES Canal_Distribucion(ID_Canal);
-ALTER TABLE Cliente 	       ADD CONSTRAINT gpoventa_FK  	  FOREIGN KEY (FK_GrupoVenta)  REFERENCES Grupo_Venta(ID_GrupoVenta);
-ALTER TABLE Centro_Productivo  ADD CONSTRAINT canaldist01_FK  FOREIGN KEY (FK_CanalDist)   REFERENCES Canal_Distribucion(ID_Canal);
-ALTER TABLE Familia_Material   ADD CONSTRAINT socfamilia_FK   FOREIGN KEY (FK_Sociedad)    REFERENCES Sociedad (ID_Sociedad);
-ALTER TABLE Material		   ADD CONSTRAINT matfamilia_FK   FOREIGN KEY (FK_FamMaterial) REFERENCES Familia_Material (ID_Familia);
-ALTER TABLE Venta   		   ADD CONSTRAINT matventa_FK     FOREIGN KEY (FK_Material)    REFERENCES Material (ID_Material);
-ALTER TABLE Venta   		   ADD CONSTRAINT socventa_FK     FOREIGN KEY (FK_Sociedad)    REFERENCES Sociedad (ID_Sociedad);
-ALTER TABLE Venta 			   ADD COLUMN FK_Cliente INT;
-ALTER TABLE Venta   		   ADD CONSTRAINT cliventa_FK     FOREIGN KEY (FK_Cliente)    REFERENCES Cliente (ID_Cliente);
+ALTER TABLE Material		   ADD CONSTRAINT matfamilia_FK   FOREIGN KEY (ID_FamMaterial) REFERENCES Familia_Material (ID_Familia);
 
-ALTER TABLE Canal_Distribucion DROP CONSTRAINT sociedad_FK;
-ALTER TABLE Canal_Distribucion DROP COLUMN FK_Sociedad;
+ALTER TABLE Venta   		   ADD CONSTRAINT socventa_FK     FOREIGN KEY (ID_Sociedad)    REFERENCES Sociedad (ID_Sociedad);
+ALTER TABLE Venta   		   ADD CONSTRAINT matventa_FK     FOREIGN KEY (ID_Material)    REFERENCES Material (ID_Material);
+ALTER TABLE Venta   		   ADD CONSTRAINT cliventa_FK     FOREIGN KEY (ID_Cliente)     REFERENCES Cliente (ID_Cliente);
+ALTER TABLE Venta			   ADD CONSTRAINT canaldist_FK    FOREIGN KEY (ID_Canal)       REFERENCES Canal_Distribucion (ID_Canal);
+ALTER TABLE Venta			   ADD CONSTRAINT centroprod_FK   FOREIGN KEY (ID_CentroProd)  REFERENCES Centro_Productivo (ID_CentroProd);
+ALTER TABLE Venta   	       ADD CONSTRAINT gpoventa_FK  	  FOREIGN KEY (ID_GrupoVenta)  REFERENCES Grupo_Venta (ID_GrupoVenta);
--- TablesCreation.sql
+++ TablesCreation.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Sociedad           (ID_Sociedad   INT PRIMARY KEY, Sociedad_Nombre   VARCHAR(45));
-CREATE TABLE Canal_Distribucion (ID_Canal      INT PRIMARY KEY, Canal_Nombre      VARCHAR(45), FK_Sociedad    INT);
-CREATE TABLE Grupo_Venta        (ID_GrupoVenta INT PRIMARY KEY, GrupoVenta_Nombre VARCHAR(45), FK_CanalDist   INT);
-CREATE TABLE Cliente            (ID_Cliente    INT PRIMARY KEY, Cliente_Nombre    VARCHAR(45), FK_GrupoVenta  INT);
-CREATE TABLE Familia_Material   (ID_Familia    INT PRIMARY KEY, Familia_Nombre    VARCHAR(45), FK_Sociedad    INT);
-CREATE TABLE Material 			    (ID_Material   INT PRIMARY KEY, Material_Nombre   VARCHAR(45), FK_FamMaterial INT);
-CREATE TABLE Centro_Productivo  (ID_CentroProd VARCHAR(6) PRIMARY KEY, CentroProd_Nombre VARCHAR(45), FK_CanalDist INT);
-CREATE TABLE Venta				      (ID_Pedido	   INT PRIMARY KEY AUTO_INCREMENT, Importe INT, Cantidad_UME INT, Cantidad_UMB INT, Fecha DATE, FK_Sociedad INT, FK_Material INT);  
+CREATE TABLE Canal_Distribucion (ID_Canal      INT PRIMARY KEY, Canal_Nombre      VARCHAR(45));
+CREATE TABLE Cliente            (ID_Cliente    INT PRIMARY KEY, Cliente_Nombre    VARCHAR(45));
+CREATE TABLE Grupo_Venta        (ID_GrupoVenta INT PRIMARY KEY, GrupoVenta_Nombre VARCHAR(45));
+CREATE TABLE Familia_Material   (ID_Familia    INT PRIMARY KEY, Familia_Nombre    VARCHAR(45));
+CREATE TABLE Centro_Productivo  (ID_CentroProd VARCHAR(6) PRIMARY KEY, CentroProd_Nombre VARCHAR(45));
+CREATE TABLE Material 			(ID_Material   INT PRIMARY KEY, Material_Nombre   VARCHAR(45), ID_FamMaterial INT); -- Tiene FK
+CREATE TABLE Venta				(ID_Pedido     INT PRIMARY KEY AUTO_INCREMENT, ID_Sociedad INT, ID_Material INT, ID_Cliente INT, ID_Canal INT, ID_CentroProd VARCHAR(6), ID_GrupoVenta INT, Importe INT, Cantidad_UME INT, Cantidad_UMB INT, Fecha DATE); -- Tiene FK's
